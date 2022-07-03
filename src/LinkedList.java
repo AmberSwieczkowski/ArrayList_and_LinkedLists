086905019cab21e46d21b685331da91dfba5de8d
Amber M <amberswieczkowski@pm.me>
2022-07-03 23:53:41 +0200

    Add contains method to LinkedList.

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -74,6 +74,26 @@ public class LinkedList {
     }
 
     // contains
+public Node contains(int value) {
+    if (isEmpty()) {
+        System.out.println("List is empty");
+        return null;
+    } else {
+        Node current = first;
+        int index = 0;
+        while (current.next != null) {
+            if (current.value == value) {
+                System.out.println("Found " + value + " at node " + index);
+                return current;
+            }
+            current = current.next;
+            index++;
+        }
+        System.out.println("List does not contain " + value);
+        return null;
+    }
+}
+
     // indexOf
 
     public void print() {
